4a449923e8dd6cea8cacc79749ac647ccf5e42de
teaver <jerzyx11@gmail.com>
2022-11-07 12:39:45 +0100

    -help, minor imprvs

---
+++ 7_11_22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public struct Osoba{
+            public string imie;
+            public string nazwisko;
+            public int wiek;
+        }
+
+        public static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Osoba osoba = new Osoba();
+            osoba.imie = "Foo";
+            osoba.nazwisko = "Bar";
+            osoba.wiek = 20;
+
+            Console.WriteLine(osoba.imie);
+            Console.WriteLine(osoba.nazwisko);
+            Console.WriteLine(osoba.wiek);
+
+            // SOurce code na moodle
+        }
+    }
+}
--- gif-cli/Program.cs
+++ gif-cli/Program.cs
@@ -5,12 +5,9 @@
 // Proceed if all good, display gif in CLI
 
 // NOTES
-// You can even get the frame durations with Image.GetPropertyItem(0x5100). 
-// Optional args
-// -- loop (loops CLI output forever)
-// -- ascii
-// -- blocks (default?)
-// -- color
+// todo:
+// opt args listed in [help]
+// escape to stop gif and program
 
 using System;
 using System.Collections.Generic;
@@ -28,20 +25,23 @@ namespace ConsoleApp
         }
 
         private static void gif_CLI(){
-            select_gif_file();
+            select_gif_file(true);
         }
 
-        private static void select_gif_file() {
-            Console.Clear();
-            log(0, "Paste your GIF file in this directory and input it's name.\nFile name: ");
+        private static void select_gif_file(bool clear) {
+            if(clear) Console.Clear();
+            log(0, "Paste your GIF file in this directory and input it's name.\nType -help for help commands\nFile name:");
             string? selected_gif = Console.ReadLine();
-
-            // get quality from user as well ( ascii bitmap size later )
-            // int quality = rl();
+            // Check if file exists in current dir
+            if(selected_gif == "-help"){
+                display_help();
+                select_gif_file(false);
+            }
             if(selected_gif != null){
+                log(3, selected_gif);
                 if(File.Exists(selected_gif)){
                     log(0, $"[ OK ] Selected file: {selected_gif}");
-                    
+                    // ask_for_opt_args();
                     // Get gif information (size in byes, width, height, frame count)
                     FileInfo file = new FileInfo(selected_gif);
                     Bitmap img_bmp = new Bitmap(selected_gif);
@@ -52,7 +52,7 @@ namespace ConsoleApp
                     var image_width = img_bmp.Width;
                     int frame_count = gif_image.GetFrameCount(FrameDimension.Time);
                     // Getting framerate
-                    PropertyItem frame_delay = gif_image.GetPropertyItem(0x5100);
+                    PropertyItem? frame_delay = gif_image.GetPropertyItem(0x5100);
                     int framerate = (frame_delay.Value [0] + frame_delay.Value[1] * 256) * 10;
                     // Log for testing
                     Console.WriteLine(framerate);
@@ -63,18 +63,18 @@ namespace ConsoleApp
                     Console.Clear();
 
                     // Testing
-                    generate_frames(gif_image, frame_count, framerate);
+                    render_frames(gif_image, frame_count, framerate);
                     log(0, "[ OK ] Saved frames");
                 }
                 else {
                     log(1, "[ WARN ] File not found, try again");
-                    select_gif_file();
-                    Environment.Exit(0);
+                    select_gif_file(false);
+                    return;
                 }
             }
             else log(2, "[ ERR ] Filename input is null."); return;
         }
-        public static void generate_frames(Image gif_img, int frame_count, int frame_rate){
+        public static void render_frames(Image gif_img, int frame_count, int frame_rate){
             System.IO.Directory.CreateDirectory("frames");
             log(0, $"[ OK ] Creating directory...");
             Console.CursorVisible = false;
@@ -87,11 +87,21 @@ namespace ConsoleApp
                 Console.CursorTop = 0;
                 ConsoleWriteImage(new Bitmap(gif_img));
                 // Wait until rendered
-                Thread.Sleep((400 / frame_rate));
+                Thread.Sleep((100 / frame_rate));
                 // Cleanup
                 // Console.Clear();
             }
             Console.Clear();
+        }
+        public static void display_help(){
+            log(3, "[ HELP ] Optional args");
+            log(3, "[ -ls ] Display all selectable .gif files in current directory");
+            log(3, "[ -l ] Loop output gif");
+            log(3, "[ -s ] Save output gif when finished");
+        }
+
+        public static void ask_for_opt_args(){
+            string? opts = Console.ReadLine();
         }
 
         static int[] cColors = { 0x000000, 0x000080, 0x008000, 0x008080, 0x800000, 0x800080, 0x808000, 0xC0C0C0, 0x808080, 0x0000FF, 0x00FF00, 0x00FFFF, 0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
@@ -153,13 +163,15 @@ namespace ConsoleApp
             // 0 -- default info log (cyan)
             // 1 -- warn log        (yellow)
             // 2 -- err log           (red)
-            if (log_type != 0 && log_type != 1 && log_type != 2){
+            // 3 -- help log         (green)
+            if (log_type != 0 && log_type != 1 && log_type != 2 && log_type != 3){
                 Console.WriteLine("log ERR - INVALID log() CALL");
                 return;
             }
             if(log_type == 0) Console.ForegroundColor = ConsoleColor.Cyan;
             if(log_type == 1) Console.ForegroundColor = ConsoleColor.Yellow;
             if(log_type == 2) Console.ForegroundColor = ConsoleColor.Red;
+            if(log_type == 3) Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(input);
             Console.ResetColor();
             return;
