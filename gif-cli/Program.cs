8bfb921ea2bc8d0916c89868097e2b873136f83a
teaver <jerzyx11@gmail.com>
2022-11-07 01:40:07 +0100

    all frames as files

--- gif-cli/Program.cs
+++ gif-cli/Program.cs
@@ -28,41 +28,48 @@ namespace ConsoleApp
         }
 
         private static void select_gif_file() {
+            Console.Clear();
             Log(0, "Paste your GIF file in this directory and input it's name.\nFile name: ");
             string? selected_gif = Console.ReadLine();
             if(selected_gif != null){
                 if(File.Exists(selected_gif)){
                     Log(0, $"[ OK ] Selected file: {selected_gif}");
-                    // Get gif information (size in byes, width, height, frames?)
+                    // Get gif information (size in byes, width, height, frame count)
                     
-                    // get file info
                     FileInfo file = new FileInfo(selected_gif);
                     Image gif_image = Image.FromFile(selected_gif);
                     Bitmap img = new Bitmap(selected_gif);
                     
-                    var sizeInBytes = file.Length;
-                    var imageHeight = img.Height;
-                    var imageWidth = img.Width;
-                    int frameCount = gif_image.GetFrameCount(FrameDimension.Time);
+                    var size_in_bytes = file.Length;
+                    var image_height = img.Height;
+                    var image_width = img.Width;
+                    int frame_count = gif_image.GetFrameCount(FrameDimension.Time);
                     // Log for testing
-                    Log(0, $"size in bytes: {sizeInBytes.ToString()}");
-                    Log(0, $"img height: {imageHeight.ToString()}");
-                    Log(0, $"img width: {imageWidth.ToString()}");
+                    Log(0, $"size in bytes: {size_in_bytes.ToString()}");
+                    Log(0, $"img height: {image_height.ToString()}");
+                    Log(0, $"img width: {image_width.ToString()}");
                     Log(0, "[ OK ]");
 
                     // Save single file (for now)
-                    gif_image.Save("frame.png", ImageFormat.Png);
+                    // gif_image.Save("frame.png", ImageFormat.Png);
+                    generate_frames(gif_image, frame_count, selected_gif);
                     Log(0, "[ OK ] Saved frames");
                 }
                 else {
                     Log(1, "[ WARN ] File not found, try again");
                     select_gif_file();
+                    Environment.Exit(0);
                 }
             }
             else Log(2, "[ ERR ] Filename input is null."); return;
         }
-        public static void generate_frames(int frames){
-            // save all frames as png
+        public static void generate_frames(Image gif_img, int frame_count, string selected_gif){
+            Log(0, $"frame count: {frame_count}");
+            for (int i = 0; i < frame_count; i++)
+            {
+                gif_img.SelectActiveFrame(FrameDimension.Time, i);
+                gif_img.Save($"_frame{i.ToString()}.png", ImageFormat.Png);
+            }
         }
         public static void Log(int log_type, string input) {
             // 0 -- default info log (cyan)
