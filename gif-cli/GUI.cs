47a8a0bd11e5b2aaf19a14ce83161f15fe168b68
Jerzy <jerzyx11@gmail.com>
2022-11-16 15:51:11 +0000

    refactor

--- gif-cli/GUI.cs
+++ gif-cli/GUI.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
 using Tb = Toolbox.Tools;
+using Gif_Cli = GIF_CLI.Program;
 
 namespace GUI
 {
@@ -13,13 +14,13 @@ namespace GUI
         // Get OS from the main file
         public static string OS = GIF_CLI.Program.OS;
         
-        public static int list_display_range = 3;
+        public static int range = 3;
         
         public static List<string> gif_list = new List<string>();
-        public static void Main() {
+        public static void Main(bool restart = false) {
 
             // Clean up after my spastic ctrl C's
-            Tb.ctrl_c_watcher();
+            if(!restart){ Tb.ctrl_c_watcher(); }
 
             // Check for gif files in directory
             check_for_gif_files();
@@ -35,8 +36,10 @@ namespace GUI
             do
             {
                 keyinfo = Console.ReadKey();
-
                 // Handle each key input (down arrow will write the menu again with a different selected item)
+                if(keyinfo.Key == ConsoleKey.Escape){
+                    Console.Clear(); Console.CursorVisible = true; Environment.Exit(0);
+                }
                 if (keyinfo.Key == ConsoleKey.DownArrow)
                 {
                     if (index + 1 < gif_list.Count)
@@ -56,46 +59,34 @@ namespace GUI
                 // Handle different action for the option
                 if (keyinfo.Key == ConsoleKey.Enter)
                 {
-                    // display_gif_details(gif_list[index]);
-                    Console.WriteLine("OK ENTER");
-                    index = 0;
+                    Console.Clear();
+                    Gif_Cli.eval_gif_file(gif_list[index]);
                 }
             }
-            while (keyinfo.Key != ConsoleKey.X);
-
+            while (keyinfo.Key != ConsoleKey.Escape);
             Console.ReadKey();
-
         }
         public static void check_for_gif_files(){
             string linux_gifs_path = Directory.GetCurrentDirectory();
             string windows_gifs_path = Path.GetFullPath(Path.Combine(linux_gifs_path, @"..\..\..\*.gif"));
             var curr_dir = (OS == "LINUX") ? linux_gifs_path : windows_gifs_path;
             var local_gif_files  = Directory.GetFiles(curr_dir, "*.gif");
-            Console.WriteLine("TESTING");
             // Add files to Array
             foreach (var gif_file in local_gif_files)
             {
+
                 int pos = gif_file.LastIndexOf("/") + 1;
                 // Log only "file.gif" instead of "/home/.../.../file.gif"
                 string gif_file_short = gif_file.Substring(pos, gif_file.Length - pos);
-                // Add the file to <string>List
-                gif_list.Add(gif_file_short);
-                // Tb.log(3, $"[ .gif ] {gif_file_short}");
+
+                // Check if file exists in list and if not, add it
+                if(!gif_list.Contains(gif_file_short)){
+                    gif_list.Add(gif_file_short);
+                }
             }
             // gif_list.ForEach(Console.WriteLine);
             Console.WriteLine();
         }
-
-
-        // Default action of all the gif_list. You can create more methods
-        static void WriteTemporaryMessage(string message)
-        {
-            Console.Clear();
-            Console.WriteLine(message);
-            Thread.Sleep(3000);
-            WriteMenu(gif_list, gif_list.First());
-        }
-
         static void display_gif_details(string selected_gif){
 
             Console.WriteLine();
@@ -127,18 +118,17 @@ namespace GUI
             // Cleanup + disable cursor while in Menu
             Console.Clear();
             Console.CursorVisible = false;
-
             Console.WriteLine();
             Console.WriteLine();
 
-            Tb.log(4, "Welcome to GIF-CLI, Select your gif file", true);
+            Tb.log(4, "[ GIF-CLI ] Select your gif file", true);
             Console.WriteLine();
-            
+    
             foreach (string option in gif_list)
             {
                 if (option == selectedOption)
                 {
-                    Tb.log(3, $"> {option} <", true);
+                    Tb.log(3, $"* {option} *", true);
                 }
                 else
                 {
--- gif-cli/Program.cs
+++ gif-cli/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
+
 using Gui = GUI.Gui;
 using Tb = Toolbox.Tools;
 
@@ -19,81 +20,16 @@ namespace GIF_CLI
 
         private static void gif_CLI(){
             Console.Clear();
+            
             // Check operating system before anything to ensure correct file paths
             check_OS();
-            // Run
+
+            // Display menu
             Gui.Main();
         }
-
-        private static void select_gif_file(bool clear) {
-
-            // Cleanup after a spastic Ctrl-C
-            Console.CursorVisible = true;
-            if(clear) Console.Clear();
-
-            // Get raw user input
-            log(0, "Paste your GIF file in this directory and input its name\nExample: \"drake.gif\"\nType \"help\" for available commands\nFile name:");
-            string? user_input = Console.ReadLine();
-            
-            // Check if user asked for help
-            if(user_input == "help"){
-                display_help();
-                select_gif_file(false);
-                return;
-            }
-            
-            // Check if user wants to list .gif files
-            if(user_input == "-ls"){
-                list_gif_files();
-                select_gif_file(false);
-                return;
-            }
-
-            // Check if user input is file + args or just file.gif and continue
-            if(!user_input.Any(x => Char.IsWhiteSpace(x))){
-                eval_gif_OS(user_input,OS); // user_input === selected_gif here
-            }
-            if(user_input.Any(x => Char.IsWhiteSpace(x))){
-                int first_space_user_input = user_input.IndexOf(" "); // i.e "drake.gif -l"
-                string?  opt_args = user_input.Substring(first_space_user_input + 1); // i.e "-l"
-                int args = check_for_opt_args(opt_args);
-                string? selected_gif = user_input.Substring(0, first_space_user_input); // i.e "drake.gif"
-                log(0, $"[ OK ] opt args: {opt_args}");
-                log(0, $"[ OK ] opt args: {args}");
-                eval_gif_OS(selected_gif,OS,args);
-            }
-        }
-
-        public static void eval_gif_OS(string selected_gif, string OS, int args = 0){
-            if(OS == "WINDOWS"){
-
-                // Check if file exists in directory (Windows)
-                string default_path = Directory.GetCurrentDirectory();
-                string main_folder_path = Path.GetFullPath(Path.Combine(default_path, @"..\..\..\"));
-                string selected_gif_file_win = Path.GetFullPath(Path.Combine(main_folder_path, $@"{selected_gif}"));
-                // testing
-                bool file_exists = File.Exists(selected_gif_file_win);
-                eval_gif_file(file_exists, selected_gif_file_win, args);
-            }
-            if(OS == "LINUX"){
-
-                // Check if file exists in directory (Linux)
-                bool file_exists = File.Exists(selected_gif);
-                eval_gif_file(file_exists, selected_gif, args);
-            }
-        }
-
-        public static void eval_gif_file(bool exists, string selected_gif, int args = 0)
+        public static void eval_gif_file(string selected_gif, int args = 0)
         {
-            if (!exists)
-            {
-                Console.Clear(); log(2, $"[ ERR ] Can't find file \"{selected_gif}\". try again");
-                select_gif_file(false);
-                return;
-            }
-            if (exists)
-            {
-                log(0, $"[ OK ] Selected file: {selected_gif}");
+                Tb.log(0, $"[ OK ] Selected file: {selected_gif}");
 
                 // Get gif information -- size in byes, width, height, frame count, frame delay
                 FileInfo file = new FileInfo(selected_gif);
@@ -107,71 +43,31 @@ namespace GIF_CLI
                 PropertyItem? frame_delay = gif_image.GetPropertyItem(0x5100);
                 int framerate = (frame_delay.Value[0] + frame_delay.Value[1] * 256) * 10;
 
-                log(0, $"gif frame delay: {framerate.ToString()}");
-                log(0, $"size in bytes: {size_in_bytes.ToString()}");
-                log(0, $"image size: {image_width.ToString()} x {image_height.ToString()} px");
-                log(0, "[ OK ]");
+                Tb.log(0, $"gif frame delay: {framerate.ToString()}");
+                Tb.log(0, $"size in bytes: {size_in_bytes.ToString()}");
+                Tb.log(0, $"image size: {image_width.ToString()} x {image_height.ToString()} px");
+                Tb.log(0, "[ OK ]");
                 Thread.Sleep(250);
                 Console.Clear();
 
                 // Check for opt args before rendering
                 if (args == 1) { render_frames(gif_image, frame_count, args); } // render looped
                 else { render_frames(gif_image, frame_count, 0); } //render normal
-            }
         }
-        public static void list_gif_files(){
+        // public static void display_help(){
 
-            Console.Clear();
-            string linux_gifs_path = Directory.GetCurrentDirectory();
-            string windows_gifs_path = Path.GetFullPath(Path.Combine(linux_gifs_path, @"..\..\..\*.gif"));
-            var curr_dir = (OS == "LINUX") ? linux_gifs_path : windows_gifs_path;
-            var gif_files = Directory.GetFiles(curr_dir, "*.gif");
-
-            Console.WriteLine("TESTING (126");
-            Console.WriteLine(curr_dir);
-            Console.WriteLine(windows_gifs_path);
-            Console.WriteLine(linux_gifs_path);
-            
-            foreach (var gif_file in gif_files)
-            {
-                int pos = gif_file.LastIndexOf("/") + 1;
-                // Log only "file.gif" instead of "/home/.../.../file.gif"
-                log(3, $"[ .gif ] {gif_file.Substring(pos, gif_file.Length - pos)}");
-            }
-            Console.WriteLine();
-        }
-        public static void display_help(){
-
-            // List all available commands
-            Console.Clear();
-            log(3, "[ HELP ] Options available: ");
-            Console.WriteLine();
-            log(3, "Commands available: ");
-            log(3, "[ -ls ] Display all selectable .gif files in current directory");
-            Console.WriteLine();
-            log(3, "Optional arguments available: ");
-            log(3, "[ file.gif -l ] Loop output gif");
-            log(3, "[ file.gif -s {number} ] Make gif and define its scale/quality");
-            Console.WriteLine();
-        }
-
-        public static int check_for_opt_args(string? opts){
-
-            if(opts == null){
-                log(0, "No opt args, moving on...");
-                return 0;
-            }
-            if(opts != null){
-                string[] opts_arr = opts.Split(" ");
-                foreach (var arg in opts_arr)
-                {
-                    if(arg != "-l" && arg != "-s"){ log(2, "[ ERR ] Invalid opt args"); return 0; }
-                    if (arg == "-l"){ log(3, "[ OPT ARG ] LOOP OUTPUT GIF"); return 1; }
-                    if (arg == "-s"){ log(3, "[ OPT ARG ] GIF WITH SCALE/QUALITY"); return 2; }
-                }
-            }
-            return 0;
-        }
+        //     // List all available commands
+        //     Console.Clear();
+        //     Tb.log(3, "[ HELP ] Options available: ");
+        //     Console.WriteLine();
+        //     Tb.log(3, "Commands available: ");
+        //     Tb.log(3, "[ -ls ] Display all selectable .gif files in current directory");
+        //     Console.WriteLine();
+        //     Tb.log(3, "Optional arguments available: ");
+        //     Tb.log(3, "[ file.gif -l ] Loop output gif");
+        //     Tb.log(3, "[ file.gif -s {number} ] Make gif and define its scale/quality");
+        //     Console.WriteLine();
+        // }
         public static void render_frames(Image gif_img, int frame_count, int args){
 
             Console.CursorVisible = false;
@@ -202,7 +98,8 @@ namespace GIF_CLI
             // Cleanup
             Console.Clear();
             Console.CursorVisible = true;
-            log(0, "[ OK ] Done");
+            Tb.log(0, "[ OK ] Done", true);
+            Gui.Main(true);
         }
 
         // Drawing functions
@@ -244,7 +141,7 @@ namespace GIF_CLI
         }
         public static void ConsoleWriteImage(Bitmap source)
         {
-            int sMax = 30; // (SCALE, PIXEL RATIO IN CLI)
+            int sMax = 34; // (SCALE, PIXEL RATIO IN CLI)
             decimal percent = Math.Min(decimal.Divide(sMax, source.Width), decimal.Divide(sMax, source.Height));
             Size dSize = new Size((int)(source.Width * percent), (int)(source.Height * percent));   
             Bitmap bmpMax = new Bitmap(source, dSize.Width * 2, dSize.Height);
@@ -270,25 +167,6 @@ namespace GIF_CLI
             {
                 OS = "LINUX";
             }
-        }
-
-        public static void log(int log_type, string input, bool center = false) {
-            // 0 -- default info log (cyan)
-            // 1 -- warn log        (yellow)
-            // 2 -- err log           (red)
-            // 3 -- help log         (green)
-            if (log_type != 0 && log_type != 1 && log_type != 2 && log_type != 3){
-                Console.WriteLine("log ERR - INVALID log() CALL");
-                return;
-            }
-            if(log_type == 0) Console.ForegroundColor = ConsoleColor.Cyan;
-            if(log_type == 1) Console.ForegroundColor = ConsoleColor.Yellow;
-            if(log_type == 2) Console.ForegroundColor = ConsoleColor.Red;
-            if(log_type == 3) Console.ForegroundColor = ConsoleColor.Green;
-            if(center == true) { Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (input.Length / 2)) + "}", input)); } 
-            if(!center){ Console.WriteLine(input); }
-            Console.ResetColor();
-            return;
         }
     }
 }
